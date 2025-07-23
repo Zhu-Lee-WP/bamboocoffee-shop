aa9df4ed79700aa0cef24c2a38f214fa012ec283
Zhu Lee <lf260830@gmail.com>
2025-07-23 16:49:40 +0800

    改為 redirect 模式測試

--- netlify/functions/ecpay-map-return.js
+++ netlify/functions/ecpay-map-return.js
@@ -1,47 +1,49 @@
-// 檔案路徑: netlify/functions/ecpay-map-return.js
+// 檔案路徑: netlify/functions/ecpay-map-return.js (採用重新導向模式)
 
-const querystring = require('querystring');
+exports.handler = async function(event, context) {
+  // 只處理來自綠界的 POST 請求
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
 
-exports.handler = async (event, context) => {
-  // 綠界回傳的資料是 URL-encoded，我們要先解析
-  const storeData = querystring.parse(event.body);
-  console.log('收到綠界回傳的門市資料:', storeData);
+  // process.env.URL 會是您 Netlify 網站的網址，例如 https://bamboocoffee-shop-ec.netlify.app
+  const checkoutUrl = `${process.env.URL}/checkout.html`;
 
-  // 我們只回傳一段簡單的 HTML，裡面包含一小段 JavaScript
-  const htmlResponse = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>處理中...</title>
-      <script>
-        // 當這個頁面載入時，執行以下動作
-        window.onload = function() {
-          // 準備要傳送的資料物件
-          const message = {
-            action: 'ecpayStoreSelected',
-            store: ${JSON.stringify(storeData)}
-          };
-          
-          // 將資料傳送給開啟這個視窗的父視窗 (也就是 checkout.html)
-          // '*' 表示允許傳送到任何來源，基於安全考量，正式上線時應更換為您的網站網域
-          window.opener.postMessage(message, '*');
-          
-          // 關閉自己這個彈出視窗
-          window.close();
-        };
-      </script>
-    </head>
-    <body>
-      <p>資料處理中，請稍候...</p>
-    </body>
-    </html>
-  `;
+  try {
+    // 綠界回傳的資料是 URL-encoded，我們要先解析
+    const params = new URLSearchParams(event.body);
+    const cvsStoreId = params.get('CVSStoreID');
+    const cvsStoreName = params.get('CVSStoreName');
+    const cvsStoreAddress = params.get('CVSAddress'); // 我們多取得地址，讓資訊更完整
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: htmlResponse,
-  };
+    // 如果沒有成功解析出門市ID或名稱，就導向回結帳頁並附上錯誤狀態
+    if (!cvsStoreId || !cvsStoreName) {
+        console.error('[錯誤] 無法從綠界的回傳中解析出門市資料。');
+        return { statusCode: 302, headers: { Location: `${checkoutUrl}?status=store-error` } };
+    }
+
+    // 將門市資訊編碼後，放在重新導向的 URL 參數中
+    const encodedStoreName = encodeURIComponent(cvsStoreName);
+    const encodedStoreAddress = encodeURIComponent(cvsStoreAddress || ''); // 如果地址為空，也做處理
+    const redirectUrl = `${checkoutUrl}?status=store-selected&cvsStoreId=${cvsStoreId}&cvsStoreName=${encodedStoreName}&cvsStoreAddress=${encodedStoreAddress}`;
+
+    console.log(`[日誌] 準備將使用者重新導向至: ${redirectUrl}`);
+
+    // 將使用者導回結帳頁面，並附上門市資訊
+    return {
+      statusCode: 302, // 302 是「重新導向」的標準代碼
+      headers: {
+        Location: redirectUrl,
+      },
+    };
+  } catch (error) {
+    console.error('[錯誤] ecpay-map-return function 發生嚴重錯誤:', error);
+    // 即使發生意外，也導回結帳頁並附上錯誤狀態
+    return {
+      statusCode: 302,
+      headers: {
+        Location: `${checkoutUrl}?status=server-error`,
+      },
+    };
+  }
 };
